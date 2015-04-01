504d98979ef496786e46cdc1f6138cc8ba560132
Chris C Cerami <chrisccerami@gmail.com>
2015-04-01 13:33:21 -0400

    Allow greeting and reverse pages to accept params through path variables in addition to query params

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GreetingController {
 
 	@RequestMapping("/greeting")
-	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+	public String greetingQuery(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	@RequestMapping("/greeting/{name}")
+	public String greetingRest(@PathVariable String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
--- src/main/java/hello/ReverseController.java
+++ src/main/java/hello/ReverseController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ReverseController {
 
 	@RequestMapping("/reverse")
-	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+	public String reverseQuery(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", new StringBuilder(name).reverse().toString());
+		return "reverse";
+	}
+
+	@RequestMapping("/reverse/{name}")
+	public String reverseRest(@PathVariable String name, Model model) {
 		model.addAttribute("name", new StringBuilder(name).reverse().toString());
 		return "reverse";
 	}
